692eda903faa4d3115e9e830650e7e4c73fbb597
ShowzZzie <111434663+ShowzZzie@users.noreply.github.com>
2026-01-01 19:27:40 +0000

    minor openmeteo.py changes, eod

--- src/holiday_destination_finder/providers/openmeteo.py
+++ src/holiday_destination_finder/providers/openmeteo.py
@@ -1,2 +1,3 @@
 def get_weather_data(lat, lon, start_date, end_date):
-    return 5 #placeholder for weather data retrieval logic
+    url=f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&daily=temperature_2m_mean,precipitation_sum&start_date={start_date}&end_date={end_date}"
+    print(url)
