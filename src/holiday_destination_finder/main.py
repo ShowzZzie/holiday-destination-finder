bb3d2993a7b67e3db1a5363deb6b77b7ec2b30d0
ShowzZzie <111434663+ShowzZzie@users.noreply.github.com>
2026-01-03 13:04:36 +0000

    creating backbone for main.py pipeline

--- src/holiday_destination_finder/main.py
+++ src/holiday_destination_finder/main.py
@@ -1,4 +1,15 @@
 from holiday_destination_finder.providers.openmeteo import get_weather_data
+from holiday_destination_finder.providers.amadeus import get_cheapest_flight_prices
+import csv
 
-weather = get_weather_data(38.7077, -9.1363, '2026-01-13', '2026-01-20')
+weather = get_weather_data(38.7077, -9.1363, '2026-01-07', '2026-01-11')
 print(weather)
+
+prices = get_cheapest_flight_prices('WRO', 'LIS', '2026-01-07', '2026-01-11')
+print(prices)
+
+with open('data/cities.csv', 'r') as cities_csv:
+    reader = csv.reader(cities_csv)
+
+    for row in reader:
+        print(row)
---
+++ src/holiday_destination_finder/providers/amadeus.py
@@ -0,0 +1,2 @@
+def get_cheapest_flight_prices(origin, destination, start_date, end_date):
+    return 1213
