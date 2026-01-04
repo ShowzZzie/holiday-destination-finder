82c95cc98d399c2cc386c1a949f4279ace9ab7f9
ShowzZzie <111434663+ShowzZzie@users.noreply.github.com>
2026-01-04 17:43:45 +0000

    adding Amadeus authenticating code, api keys

--- src/holiday_destination_finder/providers/amadeus.py
+++ src/holiday_destination_finder/providers/amadeus.py
@@ -1,3 +1,5 @@
+import os, requests
+
 stub_prices = {
         "LIS": 150,
         "MAD": 100,
@@ -14,5 +16,45 @@ stub_prices = {
         "LGA": 700,
     }
 
+_SESSION = requests.Session()
+
+def get_amadeus_token():
+    api_key = os.getenv("AMADEUS_API_KEY")
+    api_secret = os.getenv("AMADEUS_API_SECRET")
+
+    if not api_key or not api_secret:
+        raise RuntimeError("Missing AMADEUS_API_KEY or AMADEUS_API_SECRET")
+
+
+    resp = _SESSION.post(
+        "https://test.api.amadeus.com/v1/security/oauth2/token",
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        data = {
+            "grant_type": "client_credentials",
+            "client_id": api_key,
+            "client_secret": api_secret
+        },
+        timeout = 10
+    )
+    resp.raise_for_status()
+    payload = resp.json()
+    return payload["access_token"], payload["expires_in"]
+
+
 def get_cheapest_flight_prices(origin, destination, start_date, end_date):
+
+    """
+    curl "https://test.api.amadeus.com/v1/security/oauth2/token" \
+     -H "Content-Type: application/x-www-form-urlencoded" \
+     -d "grant_type=client_credentials&client_id={client_id}&client_secret={client_secret}"
+     """
+    
+    url = "https://test.api.amadeus.com/v2/shopping/flight-offers"
+    print(get_amadeus_token())
+
     return stub_prices.get(destination, "N/A")
+
+if __name__ == "__main__":
+    print(get_cheapest_flight_prices("WRO", "CTA", "2026-01-06", "2026-01-13"))
